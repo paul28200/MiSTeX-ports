5b347656cd7af5f584961bc1a9164e124b13961f
paul28200 <60981460+paul28200@users.noreply.github.com>
2025-08-22 17:15:25 +0200

    Error on rom path

--- cores/SNES/rtl/chip/DSP/DSPn.vhd
+++ cores/SNES/rtl/chip/DSP/DSPn.vhd
@@ -423,7 +423,7 @@ begin
 	                 std_logic_vector(unsigned(PC) + ("1"&x"6BD")) when VER="011" else
 	                 std_logic_vector(unsigned(PC) + ("1"&x"D8B"));
 
-	PROG_ROM : entity work.spram generic map(13, 24, "chip/DSP/dsp11b23410_p.mif")
+	PROG_ROM : entity work.spram generic map(13, 24, "rtl/chip/DSP/dsp11b23410_p.mif")
 	port map(
 		clock		=> CLK,
 		address	=> PROG_ROM_ADDR,
@@ -439,7 +439,7 @@ begin
 	                 std_logic_vector(unsigned(RP( 9 downto 0)) + ("1"&x"000")) when VER="011" else
 	                 std_logic_vector(unsigned(RP(10 downto 0)) + ("1"&x"400"));
 
-	DATA_ROM : entity work.spram generic map(13, 16, "chip/DSP/dsp11b23410_d.mif")
+	DATA_ROM : entity work.spram generic map(13, 16, "rtl/chip/DSP/dsp11b23410_d.mif")
 	port map(
 		clock		=> CLK,
 		address	=> DATA_ROM_ADDR,
@@ -564,3 +564,4 @@ begin
 	end process;
 
 end rtl;
+
